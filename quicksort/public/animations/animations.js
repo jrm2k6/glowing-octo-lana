4665d431dc30897ea7d3aff8764f4688fa1c3a66
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2013-08-25 23:51:19 +0200

    [js] Add explanations for algorithm

--- quicksort/public/animations/animations.js
+++ quicksort/public/animations/animations.js
@@ -325,7 +325,7 @@ $(document).ready(function() {
     var functions = [
         function() {Animations.showPivot(Animations.findCellFromIndexAndIteration(3,1));},
         function() {l = Animations.iterate([8,5,1,3], l, 2);},
-        function() {l = Animations.iterate([19, 44], l, 3);},
+        function() {l = Animations.iterate([19,44], l, 3);},
         function() {Animations.showPivot(Animations.findCellFromIndexAndIteration(10,2));},
         function() {l = Animations.iterate([1], l, 4);},
         function() {l = Animations.iterate([8,5], l, 5);},
@@ -337,11 +337,29 @@ $(document).ready(function() {
         function() {Animations.mergeCells()}
     ]
 
+    var explanations = [
+            Explanation.showPivotExplanation(9),
+            Explanation.showExplanationsSubListsInf(9, [8,5,1,3]),
+            Explanation.showExplanationsSubListsSup(9, [19,44]),
+            Explanation.showPivotExplanation(3),
+            Explanation.showExplanationsSubListsInf(3, [1]),
+            Explanation.showExplanationsSubListsSup(3, [8,5]),
+            Explanation.showPivotExplanation(5),
+            Explanation.showExplanationsSubListsSup(3, [8]),
+            Explanation.showPivotExplanation(44),
+            Explanation.showExplanationsSubListsInf(44, [19]),
+            Explanation.showExplanationsMerge(),
+            Explanation.showConclusion()
+    ]
+
     Animations.initList();
     var nbClicks = 0;
     $("#next").on("click", function(){
         if (nbClicks < functions.length){
             functions[nbClicks]();
+            var textArea = $(".explanations");
+            var existingText = textArea.val();
+            $(".explanations").val(existingText + explanations[nbClicks]());
         }
         nbClicks++;
         Animations.showProgress(nbClicks);
@@ -349,6 +367,7 @@ $(document).ready(function() {
 
     $("#reset").on("click", function(){
         nbClicks = 0;
+        $(".explanations").val("");
         Animations.reset();
         Animations.initList();
         Animations.showProgress(0);
---
+++ quicksort/public/animations/explanations.js
@@ -0,0 +1,29 @@
+Explanation = function(){
+    function showExplanationPivot(pivotNumber) {
+        return "Pivot is : " + pivotNumber + ".\n";
+    }
+
+    function showExplanationsSubListsInf(pivotNumber, subListInf) {
+        return "Generation of " + subListInf.toString() + "which contains only lower values than " + pivotNumber + ".\n";
+    }
+
+    function showExplanationsSubListsSup(pivotNumber, subListSup) {
+        return "Generation of " + subListSup.toString() + "which contains only bigger values than " + pivotNumber +".\n";
+    }
+
+    function showExplanationsMerge() {
+        return "Everything is now sorted, we just have to merge back the cell to have the original list sorted by ascending order.\n"
+    }
+
+    function showConclusion() {
+        return "Voila!";
+    }
+
+    return {
+        showPivotExplanation: showExplanationPivot,
+        showExplanationsSubListsInf: showExplanationsSubListsInf,
+        showExplanationsSubListsSup: showExplanationsSubListsSup,
+        showExplanationsMerge: showExplanationsMerge,
+        showConclusion: showConclusion
+    };
+}();
